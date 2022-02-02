2d425879e43b230f69df241b682ee3b21555dd0f
sedian27 <diestrasergio@gmail.com>
2022-02-01 21:49:53 -0500

    Agregando nuevo diseño con modales

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+const PUBLIC_KEY = "02d5aabe56c6ebbc7301461783553b53";
+const MD5HASH = "9595d209d54310d6c4c460508a52e022";
+
 const API = `https://gateway.marvel.com/v1/public/comics?ts=1&apikey=${PUBLIC_KEY}&hash=${MD5HASH}&orderBy=title`;
 
 const MONTHS = [
@@ -23,11 +26,10 @@ const getData = (api) => {
 };
 
 const CREATOR_ROL = {
-  writer: '<li class="list-group-item"><strong>Writer:</strong> ',
-  editor: '<li class="list-group-item"><strong>Editor:</strong> ',
-  "penciller (cover)":
-    '<li class="list-group-item"><strong>Penciller Cover:</strong> ',
-  penciller: '<li class="list-group-item"><strong>Penciller:</strong> ',
+  writer: '<div class="col-6"><strong>Writer:</strong> ',
+  editor: '<div class="col-6"><strong>Editor:</strong> ',
+  "penciller (cover)": '<div class="col-6"><strong>Penciller Cover:</strong> ',
+  penciller: '<div class="col-6"><strong>Penciller:</strong> ',
 };
 
 const formaterDate = (date) => {
@@ -40,27 +42,69 @@ const formaterDate = (date) => {
 const fillData = (comics) => {
   let html = "";
   comics.forEach((comic) => {
-    html += '<div class="col">';
-    html += `<div class="card h-100 p-2">`;
-    html += `<img src="${comic.thumbnail.path}.${comic.thumbnail.extension}" class="card-img-top" alt="${comic.title}">`;
-    html += '<div class="card-body fs-5">';
-    html += `<h5 class="card-title fs-2">${comic.title}</h5>`;
-    html += `<p class="card-text">${
-      comic.description ? comic.description : ""
-    }</p>`;
-    html += "</div>";
-    html += '<ul class="list-group list-group-flush">';
+    html += `
+    <div
+      class="modal fade"
+      id="m-${comic.id}"
+      aria-hidden="true"
+      aria-labelledby="m-${comic.id}Label"
+      tabindex="-1"
+    >
+      <div class="modal-dialog modal-dialog-centered mw-50">
+        <div class="modal-content comicContent">
+          <div class="modal-header">
+            <h5 class="card-title fs-2">${comic.title}</h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <div class="d-flex">
+              <div class="d-flex align-content-center">
+                <img
+                  src="${comic.thumbnail.path}.${comic.thumbnail.extension}"
+                  class="card-img-top comicCover"
+                  alt="${comic.title}"
+                />
+              </div>
+              <div class="container">
+                <p class="card-text">
+                  ${comic.description ? comic.description : ""}
+                </p>
+                <div class="row">`;
     comic.creators.items.forEach((creator) => {
       html += CREATOR_ROL[creator.role];
-      html += `${creator.name}</li>`;
+      html += `<p>${creator.name}</p></div>`;
     });
-    html += `<li class="list-group-item"><strong>Publish:</strong> ${formaterDate(
-      comic.dates[0].date
-    )}</li>`;
-    html += "</ul>";
+    html += `
+                  <div class="col-6">
+                    <strong>Publish:</strong>
+                    <p>${formaterDate(comic.dates[0].date)}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
-    html += "</div>";
-    html += "</div>";
+  <div class="col">
+    <div class="card h-100">
+    <a
+      data-bs-toggle="modal"
+      href="#m-${comic.id}"
+      role="button"
+      >
+      <img src="${comic.thumbnail.path}.${
+      comic.thumbnail.extension
+    }" class="card-img-top" alt="${comic.title}">
+    </a>
+    </div>
+  </div>`;
   });
 
   document.getElementById("comics").innerHTML = html;
